8fac82d3a1d549de35a18909aaeee9165a63c259
xribbi <xribbi.oz@gmail.com>
2024-11-27 14:34:55 +0900

    _

--- doc/gh-auth/_.sh
+++
@@ -1,4 +0,0 @@
-#
-# 3rd step
-#
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg ] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list
